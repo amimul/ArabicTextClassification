e52210a5d5151258435da8ba2bd1acdc391c6c35
Robert Nabil (IBS) <a-ronabi@microsoft.com>
2018-05-23 21:20:56 +0200

    changed model compilation params

--- kerasLstmClassifier.py
+++ kerasLstmClassifier.py
@@ -2,8 +2,6 @@ from keras.models import Sequential
 from keras.layers.embeddings import Embedding
 from keras.layers.core import Dense
 from keras.activations import sigmoid
-from keras.optimizers import rmsprop
-from keras.losses import mse
 from keras.layers.recurrent import LSTM
 
 import numpy as np
@@ -19,7 +17,7 @@ model = Sequential()
 model.add(Embedding(reviews.getVocabSize() + 1, 64, input_length=reviews.getMaxSenLen(), mask_zero=True))
 model.add(LSTM(10, return_sequences=False))
 model.add(Dense(1, activation=sigmoid))
-model.compile(rmsprop(), mse)
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 model.summary()
 model.fit(xTrain, yTrain)
 
