7a629376d326bc4caa0713108aeacfc8eede2af4
Robert Nabil (IBS) <a-ronabi@microsoft.com>
2018-05-30 20:08:31 +0200

    use gru instead of lstm

--- kerasLstmClassifier.py
+++ kerasLstmClassifier.py
@@ -2,7 +2,7 @@ from keras.models import Sequential
 from keras.layers.embeddings import Embedding
 from keras.layers.core import Dense
 from keras.activations import sigmoid
-from keras.layers.recurrent import LSTM
+from keras.layers.recurrent import GRU
 
 import numpy as np
 from DataReader import ReviewsReader
@@ -39,7 +39,7 @@ for word, i in word_index.items():
 model = Sequential()
 ## we use mask zero as we deal with different len sentences so we pad with zeros
 model.add(Embedding(reviews.getVocabSize() + 1, EMBEDDING_DIM, weights=[embedding_matrix], input_length=reviews.getMaxSenLen(), mask_zero=True, trainable=False))
-model.add(LSTM(10, return_sequences=False))
+model.add(GRU(10, return_sequences=False))
 model.add(Dense(1, activation=sigmoid))
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 model.summary()
