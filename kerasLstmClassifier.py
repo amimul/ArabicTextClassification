49442df532e957f69092844988d5c088e29eca0f
Robert Nabil (IBS) <a-ronabi@microsoft.com>
2018-05-31 11:51:42 +0200

    epochs 5 train 89.7 | 88.5 test

--- kerasLstmClassifier.py
+++ kerasLstmClassifier.py
@@ -43,7 +43,7 @@ model.add(GRU(10, return_sequences=False))
 model.add(Dense(1, activation=sigmoid))
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 model.summary()
-model.fit(xTrain, yTrain, epochs=3, batch_size=120)
+model.fit(xTrain, yTrain, epochs=5, batch_size=120)
 
 model.save('LSTMClassifier.h5')
 print(model.evaluate(xTrain, yTrain, batch_size=100))
