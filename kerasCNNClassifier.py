e27e7cf92fef7d16f721c02442b746747e72d1a8
Robert Nabil (IBS) <a-ronabi@microsoft.com>
2018-06-01 23:46:47 +0200

    setup bigger model

--- kerasCNNClassifier.py
+++ kerasCNNClassifier.py
@@ -1,7 +1,7 @@
 from keras.models import Sequential
 from keras.layers.embeddings import Embedding
 from keras.layers.core import Dense
-from keras.layers import Conv1D, GlobalAveragePooling1D
+from keras.layers import Conv1D, GlobalAveragePooling1D, MaxPool1D
 from keras.activations import sigmoid
 from keras.layers.recurrent import GRU
 
@@ -40,7 +40,9 @@ for word, i in word_index.items():
 model = Sequential()
 ## we use mask zero as we deal with different len sentences so we pad with zeros
 model.add(Embedding(reviews.getVocabSize() + 1, EMBEDDING_DIM, weights=[embedding_matrix], input_length=reviews.getMaxSenLen(), mask_zero=False, trainable=False))
-model.add(Conv1D(64, 5, padding='same'))
+model.add(Conv1D(128, 5, padding='same', activation='relu'))
+model.add(MaxPool1D(5))
+model.add(Conv1D(50, 5, padding='same', activation='relu'))
 model.add(GlobalAveragePooling1D())
 model.add(Dense(1, activation=sigmoid))
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
