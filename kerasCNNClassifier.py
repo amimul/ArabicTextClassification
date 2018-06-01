79734dcbce5219f6115afec65727c1abdb5d2631
Robert Nabil (IBS) <a-ronabi@microsoft.com>
2018-06-02 01:11:29 +0200

    epochs 18 97.63 train | 88.9 test

--- kerasCNNClassifier.py
+++ kerasCNNClassifier.py
@@ -47,7 +47,7 @@ model.add(GlobalAveragePooling1D())
 model.add(Dense(1, activation=sigmoid))
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 model.summary()
-model.fit(xTrain, yTrain, epochs=17, batch_size=120)
+model.fit(xTrain, yTrain, epochs=18, batch_size=120)
 
 model.save('CNNClassifier.h5')
 print(model.evaluate(xTrain, yTrain, batch_size=100))
