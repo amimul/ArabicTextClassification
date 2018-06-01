5c5974a0f4ee570df6cee3bb1fbaba475a1ee1d3
Robert Nabil (IBS) <a-ronabi@microsoft.com>
2018-06-02 01:41:03 +0200

    setup - base model was trained WITHOUT dropout for 24 epochs

--- kerasCNNClassifier.py
+++ kerasCNNClassifier.py
@@ -1,7 +1,7 @@
 from keras.models import Sequential
 from keras.layers.embeddings import Embedding
 from keras.layers.core import Dense
-from keras.layers import Conv1D, GlobalAveragePooling1D, MaxPool1D
+from keras.layers import Conv1D, GlobalAveragePooling1D, MaxPool1D, Dropout
 from keras.activations import sigmoid
 from keras.layers.recurrent import GRU
 
@@ -42,29 +42,23 @@ except:
 
     np.save('emb', embedding_matrix)
 
-# model = Sequential()
-# ## we use mask zero as we deal with different len sentences so we pad with zeros
-# model.add(Embedding(reviews.getVocabSize() + 1, EMBEDDING_DIM, weights=[embedding_matrix], input_length=reviews.getMaxSenLen(), mask_zero=False, trainable=False))
-# model.add(Conv1D(128, 5, padding='same', activation='relu'))
-# model.add(MaxPool1D(5))
-# model.add(Conv1D(50, 5, padding='same', activation='relu'))
-# model.add(GlobalAveragePooling1D())
-# model.add(Dense(1, activation=sigmoid))
-# model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-# model.summary()
+model = Sequential()
+## we use mask zero as we deal with different len sentences so we pad with zeros
+model.add(Embedding(reviews.getVocabSize() + 1, 300, weights=[embedding_matrix], input_length=reviews.getMaxSenLen(), mask_zero=False, trainable=False))
+model.add(Conv1D(128, 5, padding='same', activation='relu'))
+model.add(MaxPool1D(5))
+model.add(Dropout(0.2))
+model.add(Conv1D(50, 5, padding='same', activation='relu'))
+model.add(GlobalAveragePooling1D())
+model.add(Dropout(0.2))
+model.add(Dense(1, activation=sigmoid))
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.summary()
 
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+model.load_weights('CNNClassifier_Epoch24W')
+model.fit(xTrain, yTrain, epochs=1, batch_size=128)
 
-import keras
-for i in range(21, 30):
-    print('##################')
-    print('################## EPOCH', i)
-    print('##################')
-    
-    model = keras.models.load_model('./CNNClassifier.h5')
-    model.fit(xTrain, yTrain, epochs=1, batch_size=128)
+model.save('./CNNClassifier_Epoch24_dropout0_2_oneMoreEpoch.h5')
 
-    model.save('CNNClassifier.h5')
-    print(model.evaluate(xTrain, yTrain, batch_size=128))
-    print(model.evaluate(xTest, yTest, batch_size=128))
+print(model.evaluate(xTrain, yTrain, batch_size=128))
+print(model.evaluate(xTest, yTest, batch_size=128))
