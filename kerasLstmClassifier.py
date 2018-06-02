857291cba340b420490586f27fb473f89249cd81
Robert Nabil (IBS) <a-ronabi@microsoft.com>
2018-06-02 15:51:20 +0200

    removed loading pretrained embeddings

--- kerasLstmClassifier.py
+++ kerasLstmClassifier.py
@@ -13,33 +13,9 @@ np.random.seed(1)
 reviews = ReviewsReader()
 xTrain, yTrain, xTest, yTest = reviews.readTrainTest(twoClass=True, balanced=False)
 
-
-## read and prepare embedding
-embeddings_index = {}
-with open('./wiki.ar.vec', 'r', encoding='utf8') as f:
-    n, d = map(int, f.readline().split())
-    for line in f:
-        line = line.strip()
-        values = line.split()
-        word = ' '.join(values[:-d])
-        coefs = np.asarray(values[-d:], dtype='float32')
-        embeddings_index[word] = coefs
-
-print('Found %s word vectors in embedding.' % len(embeddings_index))
-
-word_index = reviews.getTokenizerWordIndex()
-EMBEDDING_DIM = d
-embedding_matrix = np.zeros((len(word_index) + 1, EMBEDDING_DIM))
-for word, i in word_index.items():
-    embedding_vector = embeddings_index.get(word)
-    if embedding_vector is not None:
-        # words not found in embedding index will be all-zeros.
-        embedding_matrix[i] = embedding_vector
-
-
 model = Sequential()
 ## we use mask zero as we deal with different len sentences so we pad with zeros
-model.add(Embedding(reviews.getVocabSize() + 1, EMBEDDING_DIM, weights=[embedding_matrix], input_length=reviews.getMaxSenLen(), mask_zero=True, trainable=False))
+model.add(Embedding(reviews.getVocabSize() + 1, 64, input_length=reviews.getMaxSenLen(), mask_zero=True))
 model.add(GRU(100, return_sequences=True))
 model.add(GRU(50, return_sequences=False))
 model.add(Dense(1, activation=sigmoid))
